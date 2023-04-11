941150b659936025ca90da7f60bdfc756d077eac
SounakMandal <sounakmandal21@gmail.com>
2023-04-11 10:33:26 +0530

    updated communication mechanism

--- Transmitter/Communication.ino
+++ Transmitter/Communication.ino
@@ -3,23 +3,15 @@
 
 #define CAN_MESSAGE_LENGTH 8
 #define MCP2515_CS_PIN 53
-#define MCP2515_INT_PIN 2
 
 struct can_frame CANMessage;
 MCP2515 mcp2515(MCP2515_CS_PIN);
-volatile bool interruptTriggered = false;
 
 void initCAN() {
   SPI.begin();
   mcp2515.reset();
   mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
   mcp2515.setNormalMode();
-  pinMode(MCP2515_INT_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(MCP2515_INT_PIN), interruptHandler, CHANGE);
-}
-
-void interruptHandler() {
-  interruptTriggered = true;
 }
 
 void sendData(uint8_t* data, int messageId1, int messageId2) {
@@ -30,11 +22,7 @@ void sendData(uint8_t* data, int messageId1, int messageId2) {
   for (size_t index = 0; index < CAN_MESSAGE_LENGTH; index++)
     CANMessage.data[index] = data[index];
   mcp2515.sendMessage(&CANMessage);
-  if (interruptTriggered) {
-    Serial.println("First half of message sent");
-    interruptTriggered = false;
-  }
-
+  
   delay(250);
 
   // Send the second frame
@@ -43,10 +31,6 @@ void sendData(uint8_t* data, int messageId1, int messageId2) {
   for (size_t index = 0; index < CAN_MESSAGE_LENGTH; index++)
     CANMessage.data[index] = data[CAN_MESSAGE_LENGTH + index];
   mcp2515.sendMessage(&CANMessage);
-  if (interruptTriggered) {
-    Serial.println("Second half of message sent");
-    interruptTriggered = false;
-  }
-
+  
   delay(250);
 }

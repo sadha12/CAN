c0a66f400f0d3303463b3a28ea9b708d76a79d3e
SounakMandal <sounakmandal21@gmail.com>
2023-04-11 10:44:18 +0530

    Integrate HMAC

---
+++ Transmitter/Authenticate.ino
@@ -0,0 +1,2 @@
+void computeHMAC(uint8_t message, uint8_t output) {
+}
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -5,6 +5,7 @@
 // It is assumed that the data is 1 byte and only 1 AES block is required
 uint8_t plainText;
 uint8_t cipherText[AES_BLOCKLEN];
+// uint8_t HMAC[HMAC_SHA1_HASH_SIZE];
 
 // Global variables for CAN transmission
 #define MESSAGE_ID_1 0x056
@@ -20,5 +21,6 @@ void setup() {
 void loop() {
   sense(&plainText);
   encryptData(&plainText, cipherText);
+  // computeHMAC(cipherText, HMAC);
   sendData(cipherText, MESSAGE_ID_1, MESSAGE_ID_2);
 }
